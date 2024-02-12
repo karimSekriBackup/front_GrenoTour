6c03aae29e9e4e56a4970344edb934b554198271
calanfive <camille.lelan@gmail.com>
2024-02-12 10:08:26 +0100

    33_CLL_install strybk

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {Platform, StyleSheet, Text, View} from "react-native";
+export { default } from './.storybook';
 
 import {NavigationContainer} from "@react-navigation/native";
 // import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
