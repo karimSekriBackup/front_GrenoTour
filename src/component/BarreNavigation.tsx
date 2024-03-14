78cf6ff2229bcc14d924672151e96f0e4ce445e9
aymeric <rambaudaymeric@gmail.com>
2024-03-14 09:19:17 +0100

    navigation possible avec la barre navigation entre les pages

--- src/component/BarreNavigation.tsx
+++ src/component/BarreNavigation.tsx
@@ -9,13 +9,13 @@ export default function BarreNavigation(): React.JSX.Element {
         <>
             <View style={styles.BarreNavigation}>
 
-                <StraggerComposants />
+                <StraggerComposants Button_Page_1="Inscription" Button_Page_2="Connexion" Button_Page_3="InformationItineraire" Button_Page_4="" />
 
                 <GoToPageButton title="1" page="Connexion" />
                 <GoToPageButton title="go" page="Inscription" />
                 <GoToPageButton title="2" page="InformationItineraire" />
 
-                <StraggerComposants />
+                <StraggerComposants Button_Page_1="" Button_Page_2="" Button_Page_3="" Button_Page_4="" />
 
             </View>
         </>
--- src/component/NativeBase/StaggerComposants.tsx
+++ src/component/NativeBase/StaggerComposants.tsx
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { Box, useDisclose, IconButton, Stagger, HStack, Icon, Center, NativeBaseProvider } from "native-base";
 import { MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
 
-export const StraggerComposants = () => {
+interface InPageNavigate {
+  Button_Page_1: never;
+  Button_Page_2: never;
+  Button_Page_3: never;
+  Button_Page_4: never;
+  // never ???
+}
+
+export const StraggerComposants = ({ Button_Page_1, Button_Page_2, Button_Page_3, Button_Page_4 }: InPageNavigate) => {
   const {
     isOpen,
     onToggle
   } = useDisclose();
+
+  const navigation = useNavigation()
+
+  const onPressButton_1 = useCallback(() => {
+    navigation.navigate(Button_Page_1)
+  }, [])
+
+  const onPressButton_2 = useCallback(() => {
+    navigation.navigate(Button_Page_2)
+  }, [])
+
+  const onPressButton_3 = useCallback(() => {
+    navigation.navigate(Button_Page_3)
+  }, [])
+
+  const onPressButton_4 = useCallback(() => {
+    navigation.navigate(Button_Page_4)
+  }, [])
+
   return <Center >
     <Box style={{ position: "absolute" }} alignItems="center" minH="500" >
       <Stagger visible={isOpen} initial={{
@@ -37,10 +65,10 @@ export const StraggerComposants = () => {
           }
         }
       }}>
-        <IconButton mb="4" variant="solid" bg="indigo.500" colorScheme="indigo" borderRadius="full" icon={<Icon as={MaterialIcons} size="6" name="location-pin" _dark={{ color: "warmGray.50" }} color="warmGray.50" />} />
-        <IconButton mb="4" variant="solid" bg="yellow.400" colorScheme="yellow" borderRadius="full" icon={<Icon as={MaterialCommunityIcons} _dark={{ color: "warmGray.50" }} size="6" name="microphone" color="warmGray.50" />} />
-        <IconButton mb="4" variant="solid" bg="teal.400" colorScheme="teal" borderRadius="full" icon={<Icon as={MaterialCommunityIcons} _dark={{ color: "warmGray.50" }} size="6" name="video" color="warmGray.50" />} />
-        <IconButton mb="4" variant="solid" bg="red.500" colorScheme="red" borderRadius="full" icon={<Icon as={MaterialIcons} size="6" name="photo-library" _dark={{ color: "warmGray.50" }} color="warmGray.50" />} />
+        <IconButton onPress={onPressButton_4} mb="4" variant="solid" bg="indigo.500" colorScheme="indigo" borderRadius="full" icon={<Icon as={MaterialIcons} size="6" name="location-pin" _dark={{ color: "warmGray.50" }} color="warmGray.50" />} />
+        <IconButton onPress={onPressButton_3} mb="4" variant="solid" bg="yellow.400" colorScheme="yellow" borderRadius="full" icon={<Icon as={MaterialCommunityIcons} _dark={{ color: "warmGray.50" }} size="6" name="microphone" color="warmGray.50" />} />
+        <IconButton onPress={onPressButton_2} mb="4" variant="solid" bg="teal.400" colorScheme="teal" borderRadius="full" icon={<Icon as={MaterialCommunityIcons} _dark={{ color: "warmGray.50" }} size="6" name="video" color="warmGray.50" />} />
+        <IconButton onPress={onPressButton_1} mb="4" variant="solid" bg="red.500" colorScheme="red" borderRadius="full" icon={<Icon as={MaterialIcons} size="6" name="photo-library" _dark={{ color: "warmGray.50" }} color="warmGray.50" />} />
       </Stagger>
     </Box>
     <HStack alignItems="center">
