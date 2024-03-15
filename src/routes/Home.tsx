61dafdd576fb95662a0e9812738b81ab3e10f3b6
aymeric <rambaudaymeric@gmail.com>
2024-03-15 14:58:20 +0100

    avant pull | encoure de correction des erruers

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -12,8 +12,18 @@ export default function Home(): React.JSX.Element {
 
 
         <Page>
-            <View style={styles.aaa}>
-                <Text>Home Page</Text>
+            <View style={styles.Page_home}>
+                <View style={styles.Menu_haut}>
+                    <Text>menu du haut</Text>
+                </View>
+                <MapView style={styles.Map} initialRegion={{
+                    latitude: 45.166672,
+                    longitude: 5.71667,
+                    latitudeDelta: 0.2,
+                    longitudeDelta: 0.2,
+                }} />
+                {/* <Text>Token  :{token}</Text>
+            <Text>mail  :{otherData.mail}</Text> */}                <Text>Home Page</Text>
 
                 {/* Affichage des filtres transports sélectionnés (exemple test) */}
                 <Text>{JSON.stringify(transport)}</Text>
@@ -27,7 +37,19 @@ export default function Home(): React.JSX.Element {
 }
 
 const styles = StyleSheet.create({
-    aaa: {
+    Menu_haut: {
+        height: "10%",
+        width: "100%",
+        backgroundColor: "gray",
+        borderColor: "yellow",
+        borderStyle: "solid",
+        borderWidth: 2
+    },
+    Map: {
+        height: "80%",
+        width: "100%",
+    },
+    Page_home: {
         height: "100%",
         width: "100%",
         alignItems: "center",
