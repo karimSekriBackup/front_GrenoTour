cf93347d49f17d5a5d5f638e2ec7f6952a317f33
KarimDev <karim.sekri@gmail.com>
2024-03-16 14:15:24 +0100

    Affichage OK des infos de user sur la page profile

--- src/routes/Profile.tsx
+++ src/routes/Profile.tsx
@@ -1,48 +1,61 @@
-import React from "react";
-import {Text, View, StyleSheet} from "react-native";
+import React, { useEffect, useState } from "react";
+import { Text, View, StyleSheet } from "react-native";
 import BarreNavigation from "../component/BarreNavigation";
 import Page from "../component/Page";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 
-const getstoredInfosUserConnexion = async () => {
-    try {
-      const jsonValue = await AsyncStorage.getItem('my-key');
-      return jsonValue != null ? JSON.parse(jsonValue) : null;
-    } catch (e) {
-      // error reading value
-    }
-  };
+
 
 export default function Profile(): React.JSX.Element {
-    
-    
-   const dataUserConnexion = getstoredInfosUserConnexion();
 
-    console.log(dataUserConnexion)
-     return (
-         
-         <Page>
-             <View style={styles.aaa}>
-                 <Text>Profile</Text>
-             {/* <Text>Token  :{JSON.stringify(dataUserConnexion)}</Text> */}
-             
-                 
-             </View>
-         </Page>
-         
-     );
- }
- 
- const styles = StyleSheet.create({
-     aaa: {
-         height: "100%",
-         width: "100%",
-         alignItems: "center",
-         justifyContent: "flex-end",
-         backgroundColor: "#e0ffff",
-         borderColor: "red",
-         borderStyle: "solid",
-         borderWidth: 5,
-     },
- });
+    const [dataUserConnexion, setDataUserConnexion] = useState(null)
+
+
+    useEffect(() => {
+        const getstoredInfosUserConnexion = async () => {
+            try {
+                const jsonValue = await AsyncStorage.getItem('my-jsonValueOfUserConnected');
+
+                console.log("jsonValue", jsonValue);
+
+                if (jsonValue != null) {
+                    setDataUserConnexion(JSON.parse(jsonValue))
+                }
+            } catch (e) {
+                // error reading value
+            }
+        };
+        getstoredInfosUserConnexion();
+
+    }, [])
+
+    return (
+
+        <Page>
+            <View style={styles.aaa}>
+                <Text>Profile</Text>
+                <Text>Token  :{JSON.stringify(dataUserConnexion.token)}</Text>
+                <Text>Pseudo :{JSON.stringify(dataUserConnexion.Pseudo)}</Text>
+                <Text>mail  :{JSON.stringify(dataUserConnexion.mail)}</Text>
+                <Text>telephone  :{JSON.stringify(dataUserConnexion.telephone)}</Text>
+
+
+            </View>
+        </Page>
+
+    );
+}
+
+const styles = StyleSheet.create({
+    aaa: {
+        height: "100%",
+        width: "100%",
+        alignItems: "center",
+        justifyContent: "flex-end",
+        backgroundColor: "#e0ffff",
+        borderColor: "red",
+        borderStyle: "solid",
+        borderWidth: 5,
+    },
+});
